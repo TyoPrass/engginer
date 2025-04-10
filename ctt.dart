50f4430ce6f891b03bda5006a828500576f80463
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-04-10 14:24:58 +0700

    modifikasi

--- ctt.dart
+++ ctt.dart
@@ -1,2 +1,11 @@
 // 
 // dart run flutter_native_splash:create # perintah buat jalanin splash screen
+
+// flutter_native_splash:
+//   color: "#ffffff" # Warna latar belakang (gunakan kode warna HEX)
+//   background_image: "assets/background.png" # Gambar latar belakang
+//   image: "assets/splash.png" # Gambar utama/logo
+//   android_gravity: center # Posisi gambar di Android
+//   ios_content_mode: center # Posisi gambar di iOS
+//   web_image_mode: center # Posisi gambar di Web
+//   fullscreen: true # Menyembunyikan notification bar
