41c50ae597e71935adf428a15982a60f8b3dae77
TyoPrass <220441100072@student.trunojoyo.ac.id>
2025-04-10 13:39:42 +0700

    coba dikontrakan

---
+++ ctt.dart
@@ -0,0 +1,2 @@
+// 
+// dart run flutter_native_splash:create # perintah buat jalanin splash screen
--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
+
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Halo Login Page')));
+    return Scaffold(
+      body: SafeArea(
+          child: Row(
+        children: [
+
+        ],
+      )),
+    );
   }
 }
